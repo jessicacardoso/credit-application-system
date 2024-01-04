873d9ca615d0aba64bdbac036909c381db0a49b8
Jessica Cardoso <jessicasousa.pc@gmail.com>
2024-01-03 21:19:29 -0300

    aula: Teste de Integração: CustomerResource - update

--- src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -2,6 +2,7 @@ package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerUpdateDto
 import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
@@ -138,7 +139,7 @@ class CustomerResourceTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("1000.0"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("000000"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua da Cami, 123"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+//            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
             .andDo(MockMvcResultHandlers.print())
     }
 
@@ -200,6 +201,56 @@ class CustomerResourceTest {
             .andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should update customer by id and return 200 status`() {
+        // given
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+        // when
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("$URL?customerId=${customer.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            // then
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("CamiUpdate"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("CavalcanteUpdate"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value(customer.cpf))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(customer.email))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("5000.0"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("45656"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua Updated"))
+//            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not update customer with invalid id and return 400 status`() {
+        // given
+        val invalidId: Long = Random().nextLong()
+        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+        // when
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("$URL?customerId=$invalidId")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            // then
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("class me.dio.credit.application.system.exception.BusinessException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
     private fun builderCustomerDto(
         firstName: String = "Cami",
         lastName: String = "Cavalcante",
@@ -216,6 +267,20 @@ class CustomerResourceTest {
         email = email,
         income = income,
         password = password,
+        zipCode = zipCode,
+        street = street
+    )
+
+    private fun builderCustomerUpdateDto(
+        firstName: String = "CamiUpdate",
+        lastName: String = "CavalcanteUpdate",
+        income: BigDecimal = BigDecimal.valueOf(5000.0),
+        zipCode: String = "45656",
+        street: String = "Rua Updated"
+    ): CustomerUpdateDto = CustomerUpdateDto(
+        firstName = firstName,
+        lastName = lastName,
+        income = income,
         zipCode = zipCode,
         street = street
     )
